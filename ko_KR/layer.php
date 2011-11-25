a8aa909059c00139ea4ef3555fd9ab7dd6fcdf51
Jungkwan Kim <maximumjk@gmail.com>
2011-11-25 20:52:51 +0900

    Update ko_KR/layer.php

--- ko_KR/layer.php
+++ ko_KR/layer.php
@@ -1,4 +1,20 @@
 <?php
 	$lang = array(
-	);
+		'layer_color' => array(
+		'length' => '컬러필드 입력은 6자 이상이어야 합니다.',
+		'required' => '컬러필드를 입력해주세요.',
+	),
+	'layer_file' => array(
+		'type' => '다음의 파일 포맷만 사용이 가능합니다. .KMZ, .KML 이 아닐 경우 파일이  나타나지 않습니다..',
+		'valid' => '허용되지 않은 파일 포맷의 경우 파일이 나타나지 않습니다.',
+	),
+	'layer_name' => array(
+		'length' => '이름은 최소3이상 80자 이하여야 합니다.',
+		'required' => '이름을 입력해주세요.',
+	),
+	'layer_url' => array(
+		'atleast' => 'KML Url 또는  File 이 필요합니다.',
+		'both' => 'KML file 과 url을 동시에 사용할 수 없습니다.',
+		'url' => '정확한 Url을 입력해주세요. 예시. http://www.ushahidi.com/layerl.kml',
+	));
 ?>
